a164be922857def1dc63efecd3d97c4d8162ff98
abulbashar98 <abulbashar98116@gmail.com>
2023-01-12 11:00:49 +0600

    Lesson-13 step-1, useQuestions Custom Hook Ready

--- src/Hooks/useQuestions.js
+++ src/Hooks/useQuestions.js
@@ -1,18 +1,18 @@
 import { get, getDatabase, orderByKey, query, ref } from "firebase/database";
 import { useEffect, useState } from "react";
 
-export default function useVideoList() {
+export default function useQuestions(videoID) {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
-  const [videos, setVideos] = useState(false);
+  const [questions, setQuestions] = useState([]);
   // const [hasMore, setHasMore] = useState(true);
 
   useEffect(() => {
-    async function fetchVideos() {
+    async function fetchQuestions() {
       const database = getDatabase();
-      const videosRef = ref(database, "videos");
-      const videoQuery = query(
-        videosRef,
+      const quizRef = ref(database, "quiz/" + videoID + "/questions");
+      const quizQuery = query(
+        quizRef,
         orderByKey()
         // startAt(page + ""),
         // limitToFirst(8)
@@ -22,14 +22,12 @@ export default function useVideoList() {
         setLoading(true);
         setError(false);
 
-        const snapshot = await get(videoQuery);
+        const snapshot = await get(quizQuery);
         if (snapshot.exists()) {
           setLoading(false);
-          setVideos((prevVideos) => {
-            return [prevVideos, ...Object.values(snapshot.val())];
+          setQuestions((prevQuestions) => {
+            return [prevQuestions, ...Object.values(snapshot.val())];
           });
-        } else {
-          // setHasMore(false);
         }
       } catch (error) {
         console.log(error);
@@ -39,14 +37,14 @@ export default function useVideoList() {
     }
 
     setTimeout(() => {
-      fetchVideos();
+      fetchQuestions();
     }, 3000);
-  }, []);
+  }, [videoID]);
 
   return {
     loading,
     error,
-    videos,
+    questions,
     // hasMore,
   };
 }
