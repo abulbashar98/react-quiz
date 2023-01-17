4eb841a9963710c6b2a7b26bbcf5b57dd703284f
abulbashar98 <abulbashar98116@gmail.com>
2023-01-17 13:49:52 +0600

    Lesson-15, BabyStep1:- using useRef Hook to manipulate React virtual Dom through State Toggle, set style based on state true or false on tooltip reference

--- src/Components/ProgressBar.js
+++ src/Components/ProgressBar.js
@@ -1,19 +1,37 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import classes from "../styles/ProgressBar.module.css";
 import Button from "./Button";
 
 const ProgressBar = ({ next, prev, submit, progress }) => {
+  const tooltipRef = useRef();
+  const [tooltip, setToolTip] = useState(false);
+
+  const toggleTooltip = () => {
+    if (tooltip) {
+      setToolTip(false);
+      tooltipRef.current.style.display = "none";
+    } else {
+      setToolTip(true);
+      tooltipRef.current.style.left = `calc(${progress}% - 65px)`;
+      tooltipRef.current.style.display = "block";
+    }
+  };
+
   return (
     <div className={classes.progressBar}>
       <div className={classes.blackButton} onClick={prev}>
         <span className="material-icons-outlined"> arrow_back </span>
       </div>
       <div className={classes.rangeArea}>
-        <div className={classes.tooltip}>{progress}% Complete!</div>
+        <div className={classes.tooltip} ref={tooltipRef}>
+          {progress}% Complete!
+        </div>
         <div className={classes.rangeBody}>
           <div
             className={classes.progress}
             style={{ width: `${progress}%` }}
+            onMouseOut={toggleTooltip}
+            onMouseOver={toggleTooltip}
           ></div>
         </div>
       </div>
